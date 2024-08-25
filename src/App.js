e0d7c96e2d0a7128ac75e2178dc1cee62b07025b
Prabha-2002 <163090094+Prabha-2002@users.noreply.github.com>
2024-08-25 22:28:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// src/index.js or src/App.js
 import './index.css'; // or './tailwind.css'
 import './App.css'
 import Dashboard from './Dashboard';
@@ -11,7 +9,7 @@ import AddLocation from './AddLocation';
 import LocationDetails from './LocationDetails';
 import AddHotel from './AddHotel';
 import AddPlace from './AddPlace';
-import EditPlace from './EditPlace';   // Import EditPlace component
+import EditPlace from './EditPlace';  
 import './Suggestor.css'
 import EditHotel from './EditHotel';
 import LoginForm from './LoginForm';
@@ -23,7 +21,6 @@ import Userdetails from './Userdetails';
 import TripForm from './TripForm';
 import DayFormDetails from './DayFormDetails';
 import ViewYourTripDetails from './ViewYourTripDetails';
-// import EditHotel from './EditHotel';   // Import EditHotel component
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
@@ -34,22 +31,11 @@ function App() {
 
       
       <Route path="/admin/users" element={<Userdetails />} />
-
-
-        {/* Route for the login form if authentication is required
-        {!authenticated ? (
-          <Route path="/" element={<LoginForm setAuthenticated={setAuthenticated} />} />
-        ) : (
-          <> */}
-           {/* <Route path="/" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm />} /> */}
              <Route path="/" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
             <Route path="/dashboard" element={<Dashboard />} />
 <Route path="/userdashboard" element={<Userdashboard />} />
-{/* <Route path="/details/:location" element={<Details />} /> */}
-{/* <Route path="/details/:location" element={<Details />} />
- */}
 
 <Route path="/details/:id" element={<Details />} />
                 <Route path="/tripform" element={<TripForm />} />
@@ -61,22 +47,16 @@ function App() {
             <Route path="/admin/locations/:id/add-place" element={<AddPlace />} />
             <Route path="/admin/locations/:id/add-hotel" element={<AddHotel />} />
             <Route path="/admin/places/:id/edit/:locationId" element={<EditPlace />} />
-{/* <Route path="/admin/hotels/:id/edit" element={<EditHotel />} />
- */}
                  <Route path="/trip-form/:tripId/day/:dayFormId" element={<DayFormDetails />} />
 
  <Route path="/admin/hotels/:id/:locationId" element={<EditHotel />} />
 
-{/* <Route path="/admin/locations/:locationId/hotels/:id/edit" element={<EditHotel />} /> */}
 
-                        <Route path="/admin/places/:id/edit" element={<EditPlace />} />  {/* Route for editing places */}
+                        <Route path="/admin/places/:id/edit" element={<EditPlace />} />  
 
             <Route path="/register" element={<RegisterForm />} />
             <Route path="/trip-form/:id" element={<DayFormDetails />} />
             <Route path="/view-your-trip-details/:tripId" element={<ViewYourTripDetails />} />
-
-          {/* </> */}
-        {/* )} */}
       </Routes>
     </Router>
   );
